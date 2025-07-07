6adbcbc05699c33df5f61d93e2cc435a606632f0
eriantizahara <eriantizahara05@gmail.com>
2025-07-07 23:27:27 +0700

    menyelesaikan dashboard

---
+++ resources/views/layouts/formdashboard.blade.php
@@ -0,0 +1,15 @@
+{{-- resources/views/dashboard.blade.php --}}
+@extends('layouts.dashboard')
+
+@section('page-heading')
+    <h1 class="text-3xl font-bold flex items-center gap-2">
+        Selamat Datang <span>👋</span>
+    </h1>
+    <p class="mt-2 text-gray-700">
+        Hai, selamat datang di dashboard Anda. Silakan gunakan menu di samping untuk mengelola data Pemesanan Tiket Wahana sesuai dengan peran Anda.
+    </p>
+@endsection
+
+@section('content')
+    {{-- Konten dashboard lain --}}
+@endsection
