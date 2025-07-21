fde3918916f72638bbd22ae01f63f5cba0e7b2a9
eriantizahara <eriantizahara05@gmail.com>
2025-07-21 17:58:10 +0700

    menyelesaikan proses api

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\Api\AuthControllerApi;
 
 // routes/api.php
 Route::post('/login', [AuthControllerApi::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-return $request->user();
+Route::middleware('auth:sanctum')->get('/customer', function (Request $request) {
+return $request->Customer();
 });
 
 
